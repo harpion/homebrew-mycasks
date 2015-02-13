43c6ee147b1c3dfc3233e7ffdc2eca4d76ab6987
Harpion <harpion@gmail.com>
2015-02-13 23:32:27 +0900

    add Sublime Text 3

---
+++ Casks/sublime-text3.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'sublime-text3' do
+  version '3065'
+  sha256 '373dbfcddab7fb288351763402361cd4045f4a9510eb32f81938d0c156d161c3'
+
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg'
+  name 'Sublime Text 3'
+  homepage 'http://www.sublimetext.com/3'
+  license :closed
+
+  app 'Sublime Text.app'
+end
